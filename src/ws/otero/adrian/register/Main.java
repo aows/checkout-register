02b777908b4a8a144920c3f804ca742edefb1c3b
Adrian Otero <aovila@gmail.com>
2014-04-30 10:28:57 -0700

    Added input file

--- src/ws/otero/adrian/register/Main.java
+++ src/ws/otero/adrian/register/Main.java
@@ -1,5 +1,10 @@
 package ws.otero.adrian.register;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main
 {
 
@@ -10,11 +15,45 @@ public class Main
 
       // create a new cart and add items to it
       Cart cart = new Cart();
-      cart.addItem( Catalog.searchItem( "000001" ) );
-      cart.addItem( Catalog.searchItem( "000001" ) );
-      cart.addItem( Catalog.searchItem( "000002" ) );
+      initCartFromFile( cart );
 
       System.out.println( "Total price: " + cart.calculatePrice() );
+   }
+
+   private static void initCartFromFile(Cart cart) {
+      BufferedReader br = null;
+      String line;
+
+      try
+      {
+         br = new BufferedReader( new FileReader( "input.csv" ) );
+         while( ( line = br.readLine() ) != null )
+         {
+            cart.addItem( Catalog.searchItem( line ) );
+         }
+      }
+      catch( FileNotFoundException e )
+      {
+         e.printStackTrace();
+      }
+      catch( IOException e )
+      {
+         e.printStackTrace();
+      }
+      finally
+      {
+         if( br != null )
+         {
+            try
+            {
+               br.close();
+            }
+            catch( IOException e )
+            {
+               e.printStackTrace();
+            }
+         }
+      }
    }
 
 }
